c99dc7353ec7259c9181e6e407cb50141e3b042e
gmwlqnwl <51222786+gmwlqnwl@users.noreply.github.com>
2020-06-03 20:39:23 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1562,7 +1562,7 @@ while True:
 				result = wks.acell(basicSetting[16]).value
 
 				embed = discord.Embed(
-						description= '```' + SearchID + ' 님이 받을 다이야는 ' + result + ' 다이야 입니다.```',
+						description= '```' + SearchID + ' 님이 받을 다이아는 ' + result + ' 다이아 입니다.```',
 						color=0xff00ff
 						)
 				await ctx.send(embed=embed, tts=False)
